70678cf7d7ef787053546a76fc6c99ce6db708c8
SumitKumarGodwan <2018uec1062@mnit.ac.in>
2022-07-28 15:04:02 +0530

    init commit

---
+++ src/components/News.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import NewsItems from './NewsItems'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+
+        This is a news components
+        <NewsItems/>
+        
+      </div>
+    )
+  }
+}
+
+export default News
---
+++ src/components/NewsItems.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItems extends Component {
+  render() {
+    return (
+      <div>
+        This ia a news NewsItems
+      </div>
+    )
+  }
+}
+
+export default NewsItems
